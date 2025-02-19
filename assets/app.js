d193e96650b1c8a347a9f42e313cdb59962f73e0
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2025-02-19 15:30:10 +0000

    fix tabulator

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
-import 'bootstrap';
 import Plotly from 'plotly.js-dist';
-import Tabulator from 'tabulator-tables';
+import { Tabulator } from 'tabulator-tables';
 import './styles/app.css';
+
+window.Tabulator = Tabulator;
