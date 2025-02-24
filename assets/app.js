4a00f34c12ba17d9a892cda0d4ba1462dc01e493
Nuno Lopes <nuno.lopes@tecnico.ulisboa.pt>
2025-02-24 21:19:57 +0000

    fix dashboard table

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,18 @@
 import Plotly from 'plotly.js-dist';
-import { Tabulator } from 'tabulator-tables';
+import { TabulatorFull as Tabulator } from 'tabulator-tables';
 import './styles/app.css';
 
 window.Tabulator = Tabulator;
+
+function toggleVideo(button) {
+  let videoContainer = button.nextElementSibling;
+  if (videoContainer.style.display === "none" ) {
+    videoContainer.style.display = "block";
+    button.textContent = "Hide Video";
+  } else {
+    videoContainer.style.display = "none";
+    button.textContent = "Show Video";
+  }
+}
+
+window.toggleVideo = toggleVideo;
